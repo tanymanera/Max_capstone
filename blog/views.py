4f421a23c1c1b8949c6da0164c5a79641d6c25af
tanymanera <gaetanomanera@outlook.it>
2021-08-07 06:03:18 +0200

    Les.62 Single Post html page

--- blog/views.py
+++ blog/views.py
@@ -13,4 +13,4 @@ def posts(request):
     return render(request, "blog/all-posts.html")
 
 def post_detail(request, slug):
-    return HttpResponse(f'put here post with slug: {slug}')
+    return render(request, "blog/post-detail.html")
