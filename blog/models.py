8e2a3a0ac53f9b7122d23b90e28a24b076aa0a0a
tanymanera <gaetanomanera@outlook.it>
2021-08-20 21:07:53 +0200

    creato models.py con i 3 modelli: Post, Autor, Tag

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+from datetime import datetime
+
+from django.db.models.deletion import CASCADE, SET_NULL
+from django.db.models.fields import TextField
+from django.core.validators import MinLengthValidator
+
 # Create your models here.
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, on_delete=SET_NULL, related_name='posts')
+    tags = models.models.ManyToManyField(Tag)
